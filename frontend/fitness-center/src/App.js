ca482d586be02270d9bf687921efacfaf0104eac
imrangadwal10 <imrangadwal777@gmail.com>
2022-11-09 21:14:41 +0530

    added day-1 tasks

--- frontend/fitness-center/src/App.js
+++ frontend/fitness-center/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Workout from './Pages/Workouts/Workout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+    {/* </header> */}
+    <Workout/>
     </div>
   );
 }
