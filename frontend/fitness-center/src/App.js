5b9ade6badc2e954780496e818cc9f9ce9508035
imrangadwal10 <imrangadwal777@gmail.com>
2022-11-09 21:33:56 +0530

    added day-2 tasks

--- frontend/fitness-center/src/App.js
+++ frontend/fitness-center/src/App.js
@@ -1,11 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
 import Workout from './Pages/Workouts/Workout';
 
 function App() {
   return (
-    <>
+    <div className="App">
     <Workout/>
-    </>
+    </div>
   );
 }
 
