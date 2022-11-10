47b22bc0aa9d9ba32888cf14850c8df35ec43c3e
iam-souvik <souvikpatramax@gmail.com>
2022-11-10 21:46:48 +0530

    day3_task_souvik

--- frontend/fitness-center/src/App.js
+++ frontend/fitness-center/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Store from './Pages/Store/Store';
+// import Store from './Pages/Store/Store';
+import Footer from "./Pages/Footer/Footer"
+// import Home_page from './Pages/home_page/Home_page';
 
 function App() {
   return (
     <div className="App">
-      <Store />
+      {/* <Home_page/> */}
+      {/* <Store /> */}
+      <Footer/>
     </div>
   );
 }
