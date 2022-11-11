0a3aabfab2d0fa7471ee96610604ff57730852f7
iam-souvik <souvikpatramax@gmail.com>
2022-11-11 21:14:54 +0530

    day_4_task

--- frontend/fitness-center/src/App.js
+++ frontend/fitness-center/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Store from './Pages/Store/Store';
 import Home_page from './Pages/home_page/Home_page';
 import Navbar_main from './Components/homepage/navbar_main/Navbar_main';
+import Footer from './Pages/Footer/Footer';
+import Bill_Page from './Pages/Billing/Bill_Page';
 
 function App() {
   return (
     <>
-    <Navbar_main/>
-      <Home_page/>
-      <Store/>
+    {/* <Navbar_main/> */}
+      {/* <Home_page/> */}
+      {/* <Store/> */}
+      <Bill_Page/>
+      <Footer/>
+     
     </>
 
   );
