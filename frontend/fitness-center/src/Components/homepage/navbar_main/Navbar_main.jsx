98e07d67020925829805a5b1ecdf04b68f43175b
iam-souvik <souvikpatramax@gmail.com>
2022-11-12 21:39:33 +0530

    day5_done

--- frontend/fitness-center/src/Components/homepage/navbar_main/Navbar_main.jsx
+++ frontend/fitness-center/src/Components/homepage/navbar_main/Navbar_main.jsx
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Box, Center, Flex, Image, Square, Text } from '@chakra-ui/react'
-import MainLogo from "../HomePageAssets/fitness_center_main_logo-no_bg.png"
 
-const Navbar_main = () => {
-  return (
-    <Flex color='white'>
-    <Box bg='blue.500' w='74px'>
-      <Text>Box 2</Text>
-    </Box>
-    <Center w='150px' bg='green.500'>
-      <Box>
-        <Image src={MainLogo} alt="mainlogo" />
-      </Box>
-    </Center>
-    <Square bg='blue.500' size='100px'>
-      <Text>Box 2</Text>
-    </Square>
-    <Box flex='1' bg='tomato'>
-      <Text>Box 3</Text>
-    </Box>
-  </Flex>
-  )
-}
-
-export default Navbar_main
-=======
 import React, { useState } from "react";
 import {
   Box,
@@ -765,4 +738,4 @@ const Navbar_main = () => {
 };
 
 export default Navbar_main;
->>>>>>> 9f6a3cb8ae3195dd5b92afcae9734deb981ff1a5
+
--- frontend/fitness-center/src/index.js
+++ frontend/fitness-center/src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import store from "./api";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <BrowserRouter>
      <ChakraProvider>
       <App />
     </ChakraProvider>
+    </BrowserRouter>
   </Provider>
    
   
